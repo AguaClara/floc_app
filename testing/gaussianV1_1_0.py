264b6c988e32de4c8e9964e74f8f9f6e3809ad11
Ryan <rk635@cornell.edu>
2018-03-08 19:36:18 -0500

    Writing gaussian filter

---
+++ testing/gaussianV1_1_0.py
@@ -0,0 +1,7 @@
+#In this snippet we attempt to replicate the methods in:
+#https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4752185/
+#We first start to test how the gaussian filter affects the floc images
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
