70d3fc609e33e87791dd4f47e578e33c3f87e817
Ryan <rk635@cornell.edu>
2018-03-08 19:02:07 -0500

    changed image path

--- testing/gradientTest.py
+++ testing/gradientTest.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('dave.jpg',0)
+img = cv2.imread('../images/sudoku.jpeg',0)
 
 laplacian = cv2.Laplacian(img,cv2.CV_64F)
 sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
