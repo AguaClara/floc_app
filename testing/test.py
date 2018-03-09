444afea1326223ebebe85ba95fadf708ed06ff88
Ryan <rk635@cornell.edu>
2018-03-08 20:46:22 -0500

    small aesthetic changes

--- testing/test.py
+++ testing/test.py
@@ -4,43 +4,44 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-'''img = cv2.imread("t1.jpg", cv2.IMREAD_GRAYSCALE)
+
+img = cv2.imread("t1.jpg", cv2.IMREAD_GRAYSCALE)
 #IMREAD_COLOR=1
 #IMREAD_UNCHANGED=-1
 cv2.imshow('image', img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()'''
+cv2.destroyAllWindows()
 
 
 
 #show by plt
-'''#x=[1,2,3]
-#y=[2,3,4]
-#plt.plot(x,y)
-#plt.show()
+x=[1,2,3]
+y=[2,3,4]
+plt.plot(x,y)
+plt.show()
 plt.imshow(img,cmap='gray',interpolation='bicubic')
-plt.show()'''
+plt.show()
 
 #draw/write on image
 
 #img = cv2.imread("t1.jpg", cv2.IMREAD_GRAYSCALE)
-'''draw:
+draw:
 # color : BGR green(0,255,0) white(255,255,255) black(0,0,0)
 cv2.line(img,(0,0),(150,150),(255,255,255),15)# last line width
 cv2.rectangle(img,(15,25),(300,400),(0,255,0),15)
 cv2.circle(img,(100,100),100,(234,123,100),-1)#-1: fill in the circle
 pts=np.array([[122,333],[12,45],[45,45],[64,111],[432,122]],np.int32)
-cv2.polylines(img,[pts],True,(0,255,255),3)# true: connected begin end'''
+cv2.polylines(img,[pts],True,(0,255,255),3)# true: connected begin end
 
 #write:
-'''font=cv2.FONT_HERSHEY_SIMPLEX
+font=cv2.FONT_HERSHEY_SIMPLEX
 cv2.putText(img,'opencv',(0,300),font,1,(255,100,300),2,cv2.LINE_AA)#2 thickness
 cv2.imshow('image', img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()'''
+cv2.destroyAllWindows()
 
 #image operations:
-'''img = cv2.imread("t1.jpg",1)
+img = cv2.imread("t1.jpg",1)
 img[55,55]=[255,255,255] #white pixel
 px=img[55,55]#pixel
 img[100:200,100:200]=[255,255,255]# ROI of image
@@ -49,9 +50,9 @@ face=img[300:400,200:400]
 img[0:100,0:200]=face
 cv2.imshow('image', img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()'''
+cv2.destroyAllWindows()
 
-'''#image arithmetics and logic:
+#image arithmetics and logic:
 img1=cv2.imread('t1.jpg')
 img2=cv2.imread('test.png')
 #add=img1+img2
@@ -62,7 +63,7 @@ img1gray=cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 ret,mask=cv2.threshold(img1gray,220,255,cv2.THRESH_BINARY_INV)
 cv2.imshow('mask', mask)
 cv2.waitKey(0)
-cv2.destroyAllWindows()'''
+cv2.destroyAllWindows()
 
 # thresholding:
 img=cv2.imread('../images/t1.jpg')
@@ -76,7 +77,7 @@ cv2.imshow('gaus',gaus)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-'''#template matching:
+#template matching:
 img1=cv2.imread('t1.jpg')
 img1_gray=cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 template=cv2.imread('template.png',0)
@@ -88,9 +89,9 @@ for pt in zip(*loc[::-1]):
     cv2.rectangle(img1,pt,(pt[0] + w, pt[1] + h), (0,255,255), 2)
 cv2.imshow('detected',img1)
 cv2.waitKey(0)
-cv2.destroyAllWindows()'''
+cv2.destroyAllWindows()
 
-'''#feature matching:
+#feature matching:
 img1=cv2.imread('t3.jpg',1)
 img2=cv2.imread('t4.jpg',1)
 #detector
@@ -115,10 +116,10 @@ surf = cv2.xfeatures2d.SURF_create(10000)
 kp, des = surf.detectAndCompute(threshold,None)
 img2 = cv2.drawKeypoints(threshold,kp,None,(255,0,0),4)
 plt.imshow(img2)
-plt.show()'''
+plt.show()
 
 
-'''#denoise:
+#denoise:
 img = cv2.imread('t1.jpg')
 
 dst = cv2.fastNlMeansDenoisingColored(img,None,6,6,7,21)
@@ -126,4 +127,4 @@ retval,threshold=cv2.threshold(dst,190,255,cv2.THRESH_BINARY)
 
 plt.subplot(121),plt.imshow(img)
 plt.subplot(122),plt.imshow(threshold)
-plt.show()'''
+plt.show()
