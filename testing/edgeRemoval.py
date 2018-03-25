495baa6d1c7ad6d938b49598e2ca5c3afe58a3a0
Ryan <rk635@cornell.edu>
2018-03-25 14:08:56 -0400

    floodFill of boundary images

--- testing/edgeRemoval.py
+++ testing/edgeRemoval.py
@@ -1,14 +1,24 @@
-#/usr/local/bin/python
+# /usr/local/bin/python
 
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img=cv2.imread('../images/flocs/32891.jpg')
-blur = cv2.GaussianBlur(img,(5,5),0)
+img = cv2.imread('../images/flocs/Image 32891.jpg')
+blur = cv2.GaussianBlur(img, (5, 5), 0)
 retval, img2 = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY_INV)
 
-edges = cv2.Canny(img2,100,200)
-cv2.imshow('closing',edges)
+h, w = img2.shape[:2]
+mask = np.zeros((h+2, w+2), np.uint8)
+
+#edges = cv2.Canny(img2,130,200)
+
+fld_IMG = img2.copy()
+
+
+cv2.floodFill(fld_IMG, mask, (0,0), (255,255,255))
+#inverted_Flood = cv2.bitwise_not(fld_IMG)
+img_Out = cv2.bitwise_not(fld_IMG | img2)
+cv2.imshow('closing',img_Out)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
