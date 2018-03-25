c5dd4a6af1a67bb9466d5465c8cbd888178b0bf9
Ryan <rk635@cornell.edu>
2018-03-25 13:16:39 -0400

    added egde image

--- testing/edgeRemoval.py
+++ testing/edgeRemoval.py
@@ -6,3 +6,8 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('messi5.jpg',0)
 edges = cv2.Canny(img,100,200)
+
+edges = cv2.Canny(img_32891_closing,100,200)
+cv2.imshow('closing',edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
