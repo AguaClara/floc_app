40fbf9b72f9611fcb6a3adee96e7b7624e2cf355
Ryan <rk635@cornell.edu>
2018-03-26 15:50:59 -0400

    tested adaptive thresholding methods

--- testing/edgeRemoval.py
+++ testing/edgeRemoval.py
@@ -4,21 +4,50 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('../images/flocs/Image 32891.jpg')
+###### Loading image and thresholding
+img = cv2.imread('../images/flocs/Image 32373.jpg',0)
+
+#switch it to greyscale
+#img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 blur = cv2.GaussianBlur(img, (5, 5), 0)
-retval, img2 = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY_INV)
-
-h, w = img2.shape[:2]
-mask = np.zeros((h+2, w+2), np.uint8)
-
-#edges = cv2.Canny(img2,130,200)
-
-fld_IMG = img2.copy()
+#retval, t1 = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY_INV)
 
 
-cv2.floodFill(fld_IMG, mask, (), (255,255,255))
-#inverted_Flood = cv2.bitwise_not(fld_IMG)
-img_Out = cv2.bitwise_not(fld_IMG | img2)
-cv2.imshow('closing',img_Out)
+retval, t2 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+t3=cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+
+#th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+
+thresh = t3
+######
+
+print(thresh.shape)
+
+##### Closing
+kernel = np.ones((5,5),np.uint8)
+closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+edges = cv2.Canny(closed, 130, 200)
+mask = np.pad(edges, pad_width=1, mode="constant", constant_values=0)
+fld_IMG = closed.copy()
+
+
+cv2.floodFill(fld_IMG, mask, (960, 100), (255,255,255))
+inverted_Flood = cv2.bitwise_not(fld_IMG)
+img_Out = (inverted_Flood | closed)
+'''cv2.imshow('closing', closed)
+cv2.imshow('t3', t3)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()'''
+
+
+plt.subplot(231), plt.imshow(cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)), plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(232), plt.imshow(cv2.cvtColor(t2, cv2.COLOR_GRAY2RGB)), plt.title('thresholding')
+plt.xticks([]), plt.yticks([])
+plt.subplot(233), plt.imshow(cv2.cvtColor(t3, cv2.COLOR_GRAY2RGB)), plt.title('gaussian thresholding')
+plt.xticks([]), plt.yticks([])
+plt.subplot(234), plt.imshow(cv2.cvtColor(closed, cv2.COLOR_GRAY2RGB)), plt.title('closed')
+plt.xticks([]), plt.yticks([])
+plt.show()
