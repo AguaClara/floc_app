11dd297585de7d3868b0a0417c7c707ae5e843a5
Ryan <rk635@cornell.edu>
2018-04-20 14:35:56 -0400

    Moving code into flocApp.py

--- src/flocApp.py
+++ src/flocApp.py
@@ -9,6 +9,9 @@ This file holds all the relavant scripts for the FLoc App under Agua Clara
 
 # Start of Imports
 import glob
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 # End of Imports
 
 '''
@@ -30,8 +33,40 @@ def applyTo(path, fcn):
 
 '''
 Purpose:
-    - Noise Reduction
+    - Given an image of a floc, will apply a set of image proccessing Functions
+    to identify in focus flocs from the background.
 Parameters:
+    - A .jpg image read in as a grayscale image i.e cv2.imread('path',0)
 Returns:
+    - A processed .jpg image
 Raises:
+    -
 '''
+def flocID(img):
+    blur = cv2.GaussianBlur(img, (5,5), 10)
+    t = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                                    cv2.THRESH_BINARY_INV,9,7)
+    kernel = np.ones((4,4),np.uint8)
+    dilation = cv2.dilate(t,kernel,iterations = 2)
+    opened = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
+    closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
+    return closed
+
+'''
+Purpose:
+    - Simple function that can compare two given images using matplotlib
+Parameters
+    - Two .jpg images
+Returns:
+    - Opens a pyplot showing the two images
+Raises:
+    - 
+'''
+def compareImg(img1, img2):
+    plt.subplot(121),
+    plt.imshow(cv2.cvtColor(img1, cv2.COLOR_GRAY2RGB)), plt.title('Image 1')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(232),
+    plt.imshow(cv2.cvtColor(img2, cv2.COLOR_GRAY2RGB)), plt.title('Image 2')
+    plt.xticks([]), plt.yticks([])
+    plt.show()
