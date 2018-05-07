031df6344026716f1b2761f2aa5f3615b8881da8
Ryan <rk635@cornell.edu>
2018-05-07 15:50:21 -0400

    created new file

--- testing/gradientFill.py
+++ testing/gradientFill.py
@@ -7,6 +7,7 @@ Steps:
 5. Hopefully this will work?????? PLS PLS PLS WORK PLEASE WORK FOR ME PLEASE
 PRETTY PLEASE WORK!!!!!
 '''
+import cv2
 import numpy as np
 import skimage
 from skimage import io
@@ -16,14 +17,15 @@ from scipy import ndimage as ndi
 from skimage.filters import sobel
 from skimage.segmentation import watershed
 
-img = io.imread('../images/flocs/Image 32795.jpg')
-edges = canny(img, sigma=4.)
-
-markers = np.zeros_like(img)
-markers[img < 30] = 1
-markers[img > 200] = 2
-elevation_map = sobel(img)
-segmentation = watershed(img, markers)
+img = io.imread('../images/flocs/Image 32373.jpg')
+blur = cv2.GaussianBlur(img, (5,5), 10)
+t = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                                cv2.THRESH_BINARY_INV,9,7)
+kernel = np.ones((4,4),np.uint8)
+dilation = cv2.dilate(t,kernel,iterations = 2)
+opened = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
+kernel = np.ones((8,8),np.uint8)
+closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
 
 fig, (ax1,ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3),
 sharex=True, sharey=True)
@@ -31,11 +33,11 @@ ax1.imshow(img, cmap=plt.cm.gray)
 ax1.axis('off')
 ax1.set_title('Original Image', fontsize=10)
 
-ax2.imshow(edges, cmap=plt.cm.gray)
+ax2.imshow(opened, cmap=plt.cm.gray)
 ax2.axis('off')
 ax2.set_title('Edges Image', fontsize=10)
 
-ax3.imshow(segmentation, cmap=plt.cm.gray)
+ax3.imshow(closed, cmap=plt.cm.gray)
 ax3.axis('off')
 ax3.set_title('Edges Image', fontsize=10)
 
