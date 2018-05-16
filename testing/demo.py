e5ed7202a04be1bf5b4684835ace60623abeec0b
sh2439 <31328949+sh2439@users.noreply.github.com>
2018-05-15 22:31:39 -0400

    123

--- testing/demo.py
+++ testing/demo.py
@@ -5,46 +5,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.patches as mpatches
 
-
-
-
 img = cv2.imread('../images/flocs/Image 32419.jpg',0)
-# on the left border
-#img = cv2.imread('../images/flocs/Image 32406.jpg',0)
-
-
 blur = cv2.GaussianBlur(img, (5,5), 10)
 
 t2=cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,9,7)
-
-
-kernel = np.ones((4,4),np.uint8)
-dilation = cv2.dilate(t2,kernel,iterations = 2)
-
-
-
-kernel = np.ones((4,4),np.uint8)
-
-opened = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
-
-kernel = np.ones((8,8),np.uint8)
-closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
-
-cleared = closed.copy()
-cleared=clear_border(cleared)
-
-labels=label(cleared, connectivity = 1)
-
-area=[]
-print('regions number:',labels.max())
-# The properities of images
-for region in regionprops(labels):
-    area.append(region.area)
-
-print(area)
-
-
-
+t3=cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY_INV)
 
 plt.subplot(131), plt.imshow(cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)), plt.title('img')
 plt.xticks([]), plt.yticks([])
