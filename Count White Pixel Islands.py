564fbf4cd8e7aed80fbaf0ab34881694b32f7ee5
ry275 <34991491+ry275@users.noreply.github.com>
2019-02-28 14:59:26 -0500

    Count White Pixel Islands

---
+++ Count White Pixel Islands.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# Returns: the count of "white pixel [255,255,255]" islands. 
+# Requires: a 3-dimensional list "grid" that contains only two values: [255,255,255] (white pixel) and [0,0,0] (black pixel)
+def numIslands(grid):
+        islands = 0
+        for i in range(len(grid)):  
+            for j in range(len(grid[0])):
+                if (grid[i][j] == [255,255,255]).all():
+                    islands += 1
+                    part_of_island(i,j,grid)
+        return islands
+
+# Returns: nothing, but sets all connecting white pixels to black pixels.
+# Requires: a (i,j) coordinate of a white pixel for a 3-dimensional list "grid" 
+def part_of_island(i, j,grid):
+        if i < 0 or j < 0 or i == len(grid) or j == len(grid[0]) or (grid[i][j] == [0,0,0]).all():
+            return
+        else:
+            grid[i][j][0] = 0
+            grid[i][j][1] = 0
+            grid[i][j][2] = 0
+        part_of_island(i,j+1,grid)
+        part_of_island(i,j-1,grid)
+        part_of_island(i+1,j,grid)
+        part_of_island(i-1,j,grid)  
+
+
+# In[ ]:
+
+
+# Example use
+img = cv2.imread("filtered_img.jpg")
+numIslands(img)
+
