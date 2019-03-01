e182c0fa9497629e4670fad15b29ffb10cbd9207
ry275 <34991491+ry275@users.noreply.github.com>
2019-02-28 20:20:40 -0500

    Debugged the code

--- Count White Pixel Islands.py
+++ Count White Pixel Islands.py
@@ -1,29 +1,63 @@
-# Returns: the count of "white pixel [255,255,255]" islands. 
-# Requires: a 3-dimensional list "grid" that contains only two values: [255,255,255] (white pixel) and [0,0,0] (black pixel)
-def numIslands(grid):
+
+import cv2
+
+# Returns: a 2-dimensional list that contains value True or False, denoting whether the coordinate is a white pixel or not
+# Requires: a 3-dimensional list/picture 
+def whitePixels(grid):
+    res = [[True for y in range(len(grid[0]))] for x in range(len(grid))]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            curPixel = grid[i][j]
+            for rgb in curPixel:
+                 if rgb != 255:
+                        res[i][j] = False
+                        break
+    return res
+
+import sys
+
+# Returns: the count of white pixel (RGB: [255,255,255]) islands. 
+# Requires: a 3-dimensional list/picture 
+def numWhiteIslands(img):
+        
+        # Increases the recursive call stack limit to 100,000 to ensure 
+        # that the method does not give a "recursion/stack overflow" error
+        sys.setrecursionlimit(100000)
+        
+        # Shrinks img to 1/25 its original size, reducing the number of pixels the method has to iterate over
+        shrunk = cv2.resize(img,None,fx=.2, fy=.2, interpolation = cv2.INTER_CUBIC)
+        shrunk = whitePixels(shrunk)
         islands = 0
-        for i in range(len(grid)):  
-            for j in range(len(grid[0])):
-                if (grid[i][j] == [255,255,255]).all():
+        for i in range(len(shrunk)):  
+            for j in range(len(shrunk[i])):
+                if shrunk[i][j] == True:
                     islands += 1
-                    part_of_island(i,j,grid)
+                    part_of_island(i,j,shrunk)
         return islands
 
 # Returns: nothing, but sets all connecting white pixels to black pixels.
-# Requires: a (i,j) coordinate of a white pixel for a 3-dimensional list "grid" 
+# Requires: a [i][j] coordinate of a white pixel for a 3-dimensional list/picture 
 def part_of_island(i, j,grid):
-        if i < 0 or j < 0 or i == len(grid) or j == len(grid[0]) or (grid[i][j] == [0,0,0]).all():
+        if i < 0 or j < 0 or i == len(grid) or j == len(grid[i]) or (grid[i][j] == False):
             return
         else:
-            grid[i][j][0] = 0
-            grid[i][j][1] = 0
-            grid[i][j][2] = 0
+            grid[i][j] = False
         part_of_island(i,j+1,grid)
         part_of_island(i,j-1,grid)
         part_of_island(i+1,j,grid)
-        part_of_island(i-1,j,grid)  
+        part_of_island(i-1,j,grid)
+        part_of_island(i+1,j+1,grid)
+        part_of_island(i+1,j-1,grid)
+        part_of_island(i-1,j+1,grid)
+        part_of_island(i-1,j-1,grid)
 
-# Example use
+
+
+# Example usage
 img = cv2.imread("filtered_img.jpg")
-numIslands(img)
+numWhiteIslands(img)
+
+
+
+
 
