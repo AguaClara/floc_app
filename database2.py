857afba125bc4631dd7fe8d595c3212f4f798a26
Natalie Isak <43297023+natalieisak@users.noreply.github.com>
2019-10-31 16:05:25 -0400

    new version of database- still has bugs

---
+++ database2.py
@@ -0,0 +1,62 @@
+import json
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def main():
+    database = r"C:\sqlite\db\pythonsqlite.db"
+
+    sql_create_floc_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        size integer,
+                                    ); """
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create projects table
+        a= create_table(conn, sql_create_floc_table)
+
+    else:
+        print("Error! cannot create the database connection.")
+
+
+
+    sql = ''' INSERT INTO a(size)
+              VALUES(?) '''
+    cur = conn.cursor()
+    cur.execute(sql, 5)
+    print(cur.lastrowid)
+
+if __name__ == '__main__':
+    main()
