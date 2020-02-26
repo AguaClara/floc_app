c1d9b56376ab39253ac25ecaa91d3db832f4d15b
Alanna Kim <yk557@cornell.edu>
2020-02-26 16:10:47 -0500

    new branch

--- new_camera/camera.py
+++ new_camera/camera.py
@@ -16,7 +16,10 @@ import sqlite3
 import count_and_size
 import database2
 
-import matplotib
+import matplotlib.pyplot as plt
+import csv
+
+import matplotlib
 matplotlib.use('Qt5Agg')
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
