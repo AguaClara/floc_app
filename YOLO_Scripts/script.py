d29c8399ba9251d95b7b8ce4863b8e99ff5006e3
Shreehari Srinivasan <ss2732@cornell.edu>
2023-10-18 18:24:41 -0400

    Added script to size flocs using YOLO model
    Co-authored-by: Sahil Hosalli <sh2276@users.noreply.github.com>
    Co-authored-by: anya-yerramilli <anya-yerramilli@users.noreply.github.com>
    Co-authored-by: Jacqueline Hui <jeh362@users.noreply.github.com>

---
+++ YOLO_Scripts/script.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import cv2
+import numpy as np
+import os
+import pandas as pd
+
+MODEL_FILE_PATH = "best.pt"
+IMAGES = "images"
+
+model = YOLO(MODEL_FILE_PATH)
+
+# iterate through images 
+img_id = 1
+df = pd.DataFrame(columns=["id", "floc_id", "size"])
+
+for image in os.listdir(IMAGES):
+    PATH = os.path.join(IMAGES , image)
+    predict = model.predict(PATH , save = True , save_txt = True)
+
+    length = len(predict[0].masks.data)
+    for i in range(length):
+        mask = (predict[0].masks.data[i].numpy() * 255).astype("uint8")
+        size = np.sum(mask == 255)
+        cv2.imwrite(f"masks/masks_{img_id}/{i}.jpg" , mask)
+        df = df.append({"id": img_id, "floc_id": i, "size": size}, ignore_index=True)
+
+    img_id += 1
+
+df.to_csv("masks.csv", index=False)
