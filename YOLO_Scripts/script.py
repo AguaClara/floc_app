998d7c2e2019e82d8ba6bbab44d5c3c8d1133e0d
Shreehari Srinivasan <zenshreee@gmail.com>
2023-11-05 15:13:39 -0500

    Added db.py
    - SQL alchemy database
    - Modified script.py

--- YOLO_Scripts/script.py
+++ YOLO_Scripts/script.py
@@ -3,9 +3,13 @@ import cv2
 import numpy as np
 import os
 import pandas as pd
+import db
+
 
 MODEL_FILE_PATH = "model.pt"
 IMAGES = "images"
+session = db.start()
+db_ops = db.DatabaseOperations(session)
 
 model = YOLO(MODEL_FILE_PATH)
 
@@ -17,6 +21,7 @@ try:
 except:
     pass
 for image in os.listdir(IMAGES):
+    img = db_ops.add_image(image)
     PATH = os.path.join(IMAGES , image)
     predict = model.predict(PATH , save = False)
     try:
@@ -36,7 +41,13 @@ for image in os.listdir(IMAGES):
         cv2.imwrite(f"masks/masks_{img_id}/{i}.jpg" , mask)
         print(f"Saving mask {i} for image {img_id}")
         df = df._append({"id": img_id, "floc_id": i, "size": size}, ignore_index=True)
+        db_ops.add_floc(img.id, size)
 
     img_id += 1
+
+flocs = db_ops.get_flocs_by_image_name('Image 32339.jpg')
+for floc in flocs:
+    print(f"Floc ID: {floc.id}, Image ID: {floc.image_id}, Size: {floc.size}")
+db_ops.close()
 
 df.to_csv("masks.csv", index=False)
