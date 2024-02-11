ae3255a306e1552120d1f4949c8d58327fd6ec3e
caleb2234 <159649733+caleb2234@users.noreply.github.com>
2024-02-10 22:39:04 -0500

    Update App.js
    added camera

--- new_camera/floc_react_app/src/App.js
+++ new_camera/floc_react_app/src/App.js
@@ -1,12 +1,50 @@
-//import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from 'react-bootstrap/Dropdown';
 import './App.css';
-import logo from './camera_placeholder.jpeg';
+import React, { useRef, useState } from 'react';
+import Webcam from 'react-webcam';
 function App() {
+  const webcamRef = useRef(null);
+  const [imgSrc, setImgSrc] = useState(null);
+
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+    // Here you can save the image to a directory using backend logic
+    // For demonstration purpose, you can log the base64 image data
+    console.log(imageSrc);
+  };
   return (
     <div className="App">
+      <p>Hello World</p>
       <header className="Floc App">
         <div className="cameraBox">
-          <img className="camera placeholder" src={logo} alt="Camera Placeholder"/>
+          <div style={{ position: 'relative', width: 640, height: 480 }}>
+            <Webcam
+              audio={false}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              width={640}
+              height={480}
+            />
+            <button
+              style={{
+                position: 'absolute',
+                bottom: 10,
+                right: 10,
+                zIndex: 100,
+              }}
+              onClick={capture}
+            >
+              Take Picture
+            </button>
+          </div>
+          {imgSrc && (
+            <div>
+              <h2>Preview:</h2>
+              <img src={imgSrc} alt="Captured" />
+            </div>
+          )}
+      
         </div>
         <div className="exportBox">
           <p>Export Data</p>
@@ -21,6 +59,14 @@ function App() {
           <button className="Start Button">Start</button>
           <button className="End Button">End</button>
           <button className="Change Save Location">Change Save Location</button>
+          <Dropdown className="Select Camera">
+            <Dropdown.Toggle variant="success" id="dropdown_basic">
+              Select
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Camera1</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
       </header>
     </div>
