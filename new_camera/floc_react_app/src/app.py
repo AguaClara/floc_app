270a36cd7652f04c91cdffa86e7676fde20a1cf5
Alvaro Deras <ad2226@cornell.edu>
2024-03-02 14:58:09 -0500

    Update routes and implement image uploading/retrieval

--- new_camera/floc_react_app/src/app.py
+++ new_camera/floc_react_app/src/app.py
@@ -29,14 +29,15 @@ def get_images_data():
         image_dict = {
             "id": image.id,
             "name": image.name,
+            "image": image.base64_data,
             "flocs": [{"id": floc.id, "size": floc.size} for floc in image.flocs]
         }
         image_list.append(image_dict)
 
     db_ops.close()
-    return json.dumps(image_list), 200
+    return jsonify(image_list), 200
 
-@app.route("/images/<int:image_id>/data/")
+@app.route("/images/<int:image_id>/data/", methods=["GET"])
 def get_specific_image_data(image_id):
     """
     Gets the image data at for the specified image id
@@ -51,26 +52,31 @@ def get_specific_image_data(image_id):
     image_data = {
         "id": image.id,
         "name": image.name,
+        "image": image.base64_data,
         "flocs": [{"id": floc.id, "size": floc.size} for floc in image.flocs]
     }
 
     db_ops.close()
-    return json.dumps(image_data), 200
+    return jsonify(image_data), 200
 
-
-# Need to create a new column in database to represent the actual image before using upload/get image
-
-@app.route("/upload/", methods=["POST"])
+@app.route("/upload", methods=["POST"])
 def upload_image():
     """
     Uploads an image to database
     """
     body = json.loads(request.data)
-    image_name = body.get('image_name')
+    image_base64_data = body.get('image')
 
     session = start()
     db_ops = DatabaseOperations(session)
-    new_image = db_ops.add_image(image_name)
+    current_id = db_ops.get_current_image_id()
+
+    if current_id is None:
+        image_name = "image0"
+    else:
+        image_name = f"image{current_id+1}"
+    
+    new_image = db_ops.add_image(image_name, image_base64_data)
 
     response = {
         "message": "Image uploaded",
@@ -80,24 +86,44 @@ def upload_image():
     session.commit()
     db_ops.close()
 
-    return json.dumps(response), 200
+    return jsonify(response), 200
 
-#@app.route("/images/<int:image_id>/")
-#def get_image(image_id):
-  #  """
-  #  Gets the image at the specified id
-  #  """
-  #  session = start()
-  #  db_ops = DatabaseOperations(session)
-  #  image = db_ops.session.query(Image).filter_by(id=image_id).first()
+@app.route("/images/<int:image_id>/", methods=["GET"])
+def get_image(image_id):
+    """
+    Gets the image at the specified id
+    """
+    session = start()
+    db_ops = DatabaseOperations(session)
+    image = db_ops.session.query(Image).filter_by(id=image_id).first()
 
-   # if not image:
-   #     return jsonify({"error": "Image not found"}), 404
+    if not image:
+      return jsonify({"error": "Image not found"}), 404
+  
+    response = {"image": image.base64_data}
+    db_ops.close()
 
-   # return send_file(image.data, mimetype='image/jpeg')
+    return jsonify(response), 200
 
+@app.route("/images/<int:image_id>/", methods=["DELETE"])
+def delete_image(image_id):
+    """
+    Deletes the image at the specified id
+    """
+    session = start()
+    db_ops = DatabaseOperations(session)
+    
+    image = db_ops.session.query(Image).filter_by(id=image_id).first()
 
+    if not image:
+        return jsonify({"error": "Image not found"}), 404
 
+    db_ops.session.delete(image)
+    db_ops.session.commit()
+    
+    db_ops.close()
+    return jsonify({"message": "Image deleted successfully"}), 200
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
