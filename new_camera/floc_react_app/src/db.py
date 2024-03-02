648752dc39133c3de3f3d9cf2ac12f6c46b31cbf
Alvaro Deras <ad2226@cornell.edu>
2024-03-01 23:48:17 -0500

    Add db.py to src directory, edit start()

---
+++ new_camera/floc_react_app/src/db.py
@@ -0,0 +1,68 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+
+
+# Define the base class
+Base = declarative_base()
+
+class Image(Base):
+    __tablename__ = 'images'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    flocs = relationship('Floc', backref='image')
+
+class Floc(Base):
+    __tablename__ = 'flocs'
+    id = Column(Integer, primary_key=True)
+    image_id = Column(Integer, ForeignKey('images.id'))
+    size = Column(Float)
+
+# Create an engine that stores data in the local directory's
+
+def start():
+    engine = create_engine('sqlite:///floc.db')
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
+    return session
+
+class DatabaseOperations:
+    def __init__(self, session):
+        self.session = session
+
+    def add_image(self, image_name):
+        new_image = Image(name=image_name)
+        self.session.add(new_image)
+        self.session.commit()
+        return new_image
+
+    def add_floc(self, image_id, size):
+        new_floc = Floc(image_id=image_id, size=size)
+        self.session.add(new_floc)
+        self.session.commit()
+        return new_floc
+
+    def get_flocs_by_image_name(self, image_name):
+        return self.session.query(Floc).join(Image).filter(Image.name == image_name).all()
+
+    def close(self):
+        self.session.close()
+
+# Usage
+# db_ops = DatabaseOperations()
+
+# Add an image
+# image = db_ops.add_image('Image 32339.jpg')
+
+# # Add a floc to the image we just added
+# db_ops.add_floc(image.id, 150.5)
+# db_ops.add_floc(image.id, 200.5)
+# db_ops.add_floc(image.id, 250.5)
+
+# # Retrieve all flocs for a given image name
+# flocs = db_ops.get_flocs_by_image_name('Image 32339.jpg')
+# for floc in flocs:
+#     print(f"Floc ID: {floc.id}, Image ID: {floc.image_id}, Size: {floc.size}")
+
+# Close the connection when done
+# db_ops.close()
