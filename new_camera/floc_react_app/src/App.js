c5f3a65325e36191dc678c88a8f3859c61074c80
Alvaro Deras <ad2226@cornell.edu>
2024-03-01 21:52:36 -0500

    Add auto-capture mode

--- new_camera/floc_react_app/src/App.js
+++ new_camera/floc_react_app/src/App.js
@@ -4,6 +4,8 @@ import Webcam from 'react-webcam';
 function App() {
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
+  const [autoCapture, setAutoCapture] = useState(false);
+  const [autoCaptureInterval, setAutoCaptureInterval] = useState(null);
 
   const capture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -26,8 +28,41 @@ function App() {
 
 
   };
+
+  // Auto-capture mode
+  const toggleAutoCapture = () => {
+    // Stop auto-capture mode if it's currently active
+    if (autoCapture) {
+      clearInterval(autoCaptureInterval);
+      setAutoCapture(false);
+      return;
+    }
+
+    // Start auto-capture mode
+    const intervalId = setInterval(() => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setImgSrc(imageSrc);
+
+      // Send the captured image to the backend
+      fetch('http://127.0.0.1:5000/upload', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ image: imageSrc })
+      })
+        .then(response => response.json())
+        .then(data => { console.log(data); alert(JSON.stringify(data)) })
+        .catch(error => console.log(error));
+    }, 5000); // Capture an image every 5 seconds
+
+    // Set auto-capture mode to true
+    setAutoCapture(true);
+    setAutoCaptureInterval(intervalId);
+  };
+
   const fetchData = () => {
-    // Use the Fetch API to send a GET request to your Flask backend
+    // Use the Fetch API to send a GET request to Flask backend
     fetch('http://127.0.0.1:5000/test')
       .then(response => {
         if (!response.ok) {
@@ -46,6 +81,9 @@ function App() {
   return (
     <div className="container">
       <header className="Floc App">
+        <div>
+          <img src="https://res.cloudinary.com/scalefunder/image/fetch/s--7ZiRgq59--/f_auto,fl_lossy,q_auto/https://github.com/AguaClara/public_relations/blob/master/AguaClara%2520Official%2520Logo/FINAL%2520LOGO%25202.0.png%3Fraw%3Dtrue" alt = "" width={290} height={95}/>
+        </div>
         <div className="cameraBox">
           <div style={{ position: 'relative', width: 640, height: 480 }}>
             <Webcam className='video'
@@ -65,6 +103,18 @@ function App() {
               onClick={capture}
             >
               <img alt='' src='https://www.pngall.com/wp-content/uploads/13/Red-Button-PNG.png' width={30} height={30} />
+            </button>
+            
+            <button className="autoCaptureButton"
+              style={{
+                position: 'absolute',
+                bottom: 10,
+                left: 10,
+                zIndex: 100,
+              }}
+            onClick={toggleAutoCapture}>
+              <img alt='' src = 'https://cdn3.iconfinder.com/data/icons/photography-vol-1-1/66/photography-05-1024.png' width={30} height={30} />
+              {autoCapture ? 'Stop Auto-Capture' : 'Start Auto-Capture'}
             </button>
           </div>
           {imgSrc && (
