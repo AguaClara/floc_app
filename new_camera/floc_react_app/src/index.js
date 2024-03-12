ac4e95a2a13db6875781c3d72ced4e50df38ded6
caleb2234 <159649733+caleb2234@users.noreply.github.com>
2024-03-12 16:31:37 -0400

    functionality for new page

--- new_camera/floc_react_app/src/index.js
+++ new_camera/floc_react_app/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import NewPage from './NewPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/new" element={<NewPage />} />
+    </Routes>
+  </HashRouter>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
