5e51d89d4e2a6cded791221c40a3bf6212e95ce5
Becca Jeffries <beccajeffries@dhcp-vl2041-49893.eduroam.cornell.edu>
2024-04-11 17:48:51 -0400

    settings info pages updates

--- new_camera/floc_react_app/src/Setting.js
+++ new_camera/floc_react_app/src/Setting.js
@@ -1,27 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import HeaderBox from './headerBox.jsx';
 import './App.css';
-
-function ConfirmationPopup({ onConfirm }) {
-  return (
-    <div className="popup">
-      <>Are you sure you want to clear the database? </>
-      <button onClick={onConfirm}>Yes</button>
-      <button onClick={() => {}}>Cancel</button>
-    </div>
-  );
-}
+import { useState, useEffect } from 'react';
+import './app_instructions.txt'
 
 const Setting = (props) => {
   const [inputValue, setInputValue] = useState(props.countdownSecond);
   const [storageCapacity, setStorageCapacity] = useState(5000);
   const [showPopup, setShowPopup] = useState(false);
+  const [showAcInfo, setShowAcInfo] = useState(false);
+  const [showAppInfo, setShowAppInfo] = useState(false);
+  const [fileContent, setFileContent] = useState('');
 
   // Event handler to handle input changes
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
     props.setCountdownSeconds(event.target.value);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('app_instructions.txt', {
+          headers: {
+            'Content-Type': 'text/plain'
+          }
+        });
+        if (!response.ok) {
+          throw new Error('Failed to fetch file');
+        }
+        // Assuming the file is in plaintext format
+        const text = await response.text();
+        setFileContent(text);
+      } catch (error) {
+        alert(error)
+        console.error('Error fetching file:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   const handleClearDatabase = () => {
     setShowPopup(true);
@@ -30,14 +47,27 @@ const Setting = (props) => {
   const handleCancelClearDatabase = () => {
     setShowPopup(false);
   };
-/*
+
   const handleStorageInputChange = (event) => {
     setStorageCapacity(event.target.value);
   }
   const updateCountdownSeconds = () => {
     props.setCountdownSeconds(inputValue);
   };
-*/
+
+  const handleAcInfoClick = () => {
+    setShowAcInfo(true);
+  }
+
+  const handleCloseClick = () => {
+    setShowAcInfo(false);
+    setShowAppInfo(false);
+  }
+
+  const handleAppInfoClick = () => {
+    setShowAppInfo(true);
+  }
+
   const handleConfirmClearDatabase = () => {
     fetch('http://127.0.0.1:5000/images/', {
       method: 'DELETE'
@@ -84,12 +114,6 @@ const Setting = (props) => {
             <> images</>
             <p></p>
             <button className="deleteButton" onClick={handleClearDatabase}>Clear Database Images</button>
-            {showPopup && (
-              <ConfirmationPopup
-                onConfirm={handleConfirmClearDatabase}
-                onCancel={handleCancelClearDatabase}
-              />
-            )}
           </div>
         </div>
         <div className="infoHeader">
@@ -98,12 +122,40 @@ const Setting = (props) => {
           </div>
         <div className="infoBoxWrapper">
           <div className="infoBox">
-            <button className="infoButton">App Instructions and Documentation</button>
+            <button className="infoButton" onClick={handleAppInfoClick}>App Instructions and Documentation</button>
             <p></p>
-            <button className="infoButton">More about AguaClara Cornell</button>
+            <button className="infoButton" onClick={handleAcInfoClick}>More about AguaClara Cornell</button>
           </div>
         </div>
       </div>
+      {showPopup && (
+        <div>
+          <div className="popupWrapper"></div>
+          <div className="popup">
+              <p className="popupMsg">Are you sure you want to clear the database? </p>
+              <button className="popupButton" onClick={handleConfirmClearDatabase}>Yes</button>
+              <button className="popupButton" onClick={handleCancelClearDatabase}>Cancel</button>
+          </div> 
+        </div>)}
+      {showAcInfo && (
+        <div>
+          <div className = "popupWrapper"></div>
+          <div className = "acInfoBox">
+          <img className = "logo_img" src="https://aguaclara.cornell.edu/assets/AguaClara_Logo-beebcdc8.png" alt="" width={440} height={90} />
+            <p className="acMsg">Founded in 2005, AguaClara Cornell pioneers research in community-scale water treatment technologies. AguaClara Cornell has partnered with AguaClara Reach and other local organizations to build fourteen AguaClara plants that provide safe water on tap to over 65,000 people, with the flagship plant in Ojojona, Honduras and other plants in India. The research, invention, and design project courses specific to AguaClara are focused on developing technology that can be used to improve the drinking water quality of surface water sources in the Global South. The program is structured to allow students with different levels of expertise to collaborate, invent, and change the world. Our designs are meticulously created to use locally-sourced materials, ensuring they are environmentally friendly and easy to maintain for the communities we serve. This approach combines simplicity, ecological responsibility, and community empowerment into one cohesive vision.</p>
+          <button className="closeButton" onClick={handleCloseClick}>X</button>
+          </div>
+        </div>
+      )}
+      {showAppInfo && (
+        <div>
+        <div className = "popupWrapper"></div>
+        <div className = "acInfoBox">
+        <p className="acMsg">[App instructions and documentation]</p>
+        <button className="closeButton" onClick={handleCloseClick}>X</button>
+        </div>
+      </div>
+      )}
     </div>
   );
 }
